53a98fc911cb708983d102d5a2a19f17469fa7d7
fwzin <137823900+fwzin@users.noreply.github.com>
2023-09-11 11:09:53 -0300

    tinta.py

--- estrutura sequencial/tinta.py
+++ estrutura sequencial/tinta.py
@@ -8,7 +8,6 @@ comprar apenas galões de 3,6 litros;
 misturar latas e galões, de forma que o desperdício de tinta seja menor. Acrescente 10% de folga e sempre arredonde os 
 valores para cima, isto é, considere latas cheias.
 """
-#para arrendondar para cima os valores
 import math
 
 litros_por_metro_quadrado = 1 / 6
@@ -16,20 +15,20 @@ litros_por_lata = 18
 preco_lata = 80.00
 litros_por_galao = 3.6
 preco_galao = 25.00
-folga = 1.1  # 10% de folga
+folga = 1.1 
 
 print('')
 area_a_ser_pintada = float(input("Digite o tamanho da área a ser pintada em metros quadrados: "))
 
-# Calculo 18 litros
+
 quantidade_latas = math.ceil((area_a_ser_pintada * litros_por_metro_quadrado * folga) / litros_por_lata)
 preco_total_latas = quantidade_latas * preco_lata
 
-# Calculo 3,6 litros
+
 quantidade_galoes = math.ceil((area_a_ser_pintada * litros_por_metro_quadrado * folga) / litros_por_galao)
 preco_total_galoes = quantidade_galoes * preco_galao
 
-# Calculo da quantidade de tinta e preços para latas e galões combinados
+
 quantidade_combinada_latas = quantidade_latas
 quantidade_combinada_galoes = math.ceil((area_a_ser_pintada * litros_por_metro_quadrado * folga) / litros_por_galao) % math.ceil(litros_por_lata / litros_por_galao)
 preco_total_combinado = (quantidade_combinada_latas * preco_lata) + (quantidade_combinada_galoes * preco_galao)
